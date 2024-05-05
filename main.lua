18349c0cd44864b765d5b3f315569ed9d08ff493
O1u8IArUAQfrKdmZVDI2xPq9zhMbh3VCTIcwNjx <165372514+O1u8IArUAQfrKdmZVDI2xPq9zhMbh3VCTIcwNjx@users.noreply.github.com>
2024-05-05 12:31:10 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,12 +6,6 @@ elseif mode == 1 then
   print("hello world")
 elseif mode == 2 then
   print('ac')
-elseif mode == 3 then
-  getgenv().fps_cap = 3 --# FPS
-  getgenv().disable_rendering = true --# Rendering // Saves CPU
-  
-  getgenv().farm = true --# Toggle
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/polarsblade/source/main/utilities/shotgun_autofarm'))()
 elseif mode == 4 then
   loadstring(game:HttpGet('https://raw.githubusercontent.com/O1u8IArUAQfrKdmZVDI2xPq9zhMbh3VCTIcwNjx/control/main/autofarm'))()
 
