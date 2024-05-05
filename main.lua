3c80f482b9515e346425d770fceed8cb3ea61737
O1u8IArUAQfrKdmZVDI2xPq9zhMbh3VCTIcwNjx <165372514+O1u8IArUAQfrKdmZVDI2xPq9zhMbh3VCTIcwNjx@users.noreply.github.com>
2024-05-04 21:53:40 -0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,7 @@
+local mode = loadstring(game:HttpGet('https://raw.githubusercontent.com/O1u8IArUAQfrKdmZVDI2xPq9zhMbh3VCTIcwNjx/control/main/mode.lua'))()
+
+if mode == 0 then
+  return
+elseif mode == 1 then
+  print("hello world")
+end
