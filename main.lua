3f29f2865d4c0777c6baba88e6bff2d46b677d6b
O1u8IArUAQfrKdmZVDI2xPq9zhMbh3VCTIcwNjx <165372514+O1u8IArUAQfrKdmZVDI2xPq9zhMbh3VCTIcwNjx@users.noreply.github.com>
2024-05-05 20:16:15 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local mode = loadstring(game:HttpGet('https://raw.githubusercontent.com/O1u8IArUAQfrKdmZVDI2xPq9zhMbh3VCTIcwNjx/control/main/mode.lua'))()
 
-if (mode == 0) then
+if (mode == 0 or mode == 'none') then
     return
 elseif (mode == 1 or mode == 'altcontrol') then
     --ac
