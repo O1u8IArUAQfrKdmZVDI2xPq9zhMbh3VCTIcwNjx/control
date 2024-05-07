6e70fa23895b0af43929d72f8cc0d764b7230201
O1u8IArUAQfrKdmZVDI2xPq9zhMbh3VCTIcwNjx <165372514+O1u8IArUAQfrKdmZVDI2xPq9zhMbh3VCTIcwNjx@users.noreply.github.com>
2024-05-06 20:16:17 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,4 +7,6 @@ elseif (mode == 1 or mode == 'altcontrol') then
     --ac
 elseif (mode == 2 or mode == 'autofarm') then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/O1u8IArUAQfrKdmZVDI2xPq9zhMbh3VCTIcwNjx/control/main/autofarm.lua'))()
+elseif (mode == 3 or mode == 'autofarm2') then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/O1u8IArUAQfrKdmZVDI2xPq9zhMbh3VCTIcwNjx/control/main/autofarm2.lua'))()
 end
