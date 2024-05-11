1e7a4e25395607009a49a058f6b453431169a016
O1u8IArUAQfrKdmZVDI2xPq9zhMbh3VCTIcwNjx <165372514+O1u8IArUAQfrKdmZVDI2xPq9zhMbh3VCTIcwNjx@users.noreply.github.com>
2024-05-10 20:56:58 -0500

    Update autofarm.lua

--- autofarm.lua
+++ autofarm.lua
@@ -28,7 +28,7 @@ _G.configuration = {
         leaveonknock = true, -- automatically serverhops when knocked
         timer = {
             usetimer = true, -- automatically serverhops after 'seconds' passes
-            seconds = 120
+            seconds = 90
         },
         playerjoinedhop = false, -- automatically serverhops when player with matching id in 'joinedids' joins
         joinedids = {
