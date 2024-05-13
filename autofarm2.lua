e797050910b6bf6e77c11352500fa31b6d1e8dab
O1u8IArUAQfrKdmZVDI2xPq9zhMbh3VCTIcwNjx <165372514+O1u8IArUAQfrKdmZVDI2xPq9zhMbh3VCTIcwNjx@users.noreply.github.com>
2024-05-12 22:51:43 -0500

    Update autofarm2.lua

--- autofarm2.lua
+++ autofarm2.lua
@@ -23,5 +23,42 @@ _G.AutofarmSettings = {
     
     ["Credits"] = "iku autofarm - by @trans"
 }
+--\\ Services
+local Players = game:GetService('Players');
+local CoreGui = game:GetService('CoreGui')
+local RunService = game:GetService('RunService');
+local HTTPService = game:GetService('HttpService');
+local VirtualUser = game:GetService('VirtualUser');
+local TeleportService = game:GetService('TeleportService');
+local ReplicatedStorage = game:GetService('ReplicatedStorage');
+
+--\\ Variables
+local Client = Players.Player;
+local Backpack = Client.Backpack;
+local DataFolder = Client:WaitForChild('DataFolder');
+local Information = DataFolder:WaitForChild('Information');
+local register = workspace.Cashiers;
+local ignored = workspace.Ignored;
+local hospital = ignored.HospitalJob;
+local drop = ignored.Drop;
+local shop = ignored.Shop;
+
+local function Serverhop()
+    local PlaceID = game.PlaceId
+    local Servers = HTTPService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/'.. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
+    local OpenServers = {}
+    for _, Server in pairs(Servers.data) do
+        if Server.playing ~= Server.maxPlayers and Server.playerIds ~= Client.UserId then
+            table.insert(OpenServers, Server)
+        end
+    end
+
+    TeleportService:TeleportToPlaceInstance(PlaceID, OpenServers[math.random(1, #OpenServers)].id)
+end
+
+task.delay(300, function()
+    Serverhop()
+end)
+
 
 loadstring(game:HttpGet("https://github.com/applless/RandomScripts/raw/main/IkuAutofarm"))()
